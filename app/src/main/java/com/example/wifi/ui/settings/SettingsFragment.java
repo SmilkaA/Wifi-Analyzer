e5cd51f0fc71587a6fdf873c4bbcea869e38b6f9
Anastasiia Smilka <anastasiia.smilka@globallogic.com>
2023-01-30 11:21:01 +0200

    add settings view

--- app/src/main/java/com/example/wifi/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/wifi/ui/settings/SettingsFragment.java
@@ -1,37 +1,15 @@
 package com.example.wifi.ui.settings;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceFragmentCompat;
 
-import com.example.wifi.databinding.FragmentSettingsBinding;
+import com.example.wifi.R;
 
-public class SettingsFragment extends Fragment {
-
-    private FragmentSettingsBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        SettingsViewModel settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
-
-        binding = FragmentSettingsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings, rootKey);
     }
 }
