05e7d1f971f04a624983d105abb5b6e8a46f55bf
Anastasiia Smilka <anastasiia.smilka@globallogic.com>
2023-02-03 16:01:55 +0200

    customize access points listview

--- app/src/main/java/com/example/wifi/ui/access_points/AccessPointAdapter.java
+++ app/src/main/java/com/example/wifi/ui/access_points/AccessPointAdapter.java
@@ -2,6 +2,9 @@ package com.example.wifi.ui.access_points;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.net.wifi.ScanResult;
 import android.os.Build;
 import android.util.TypedValue;
@@ -9,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
@@ -65,6 +69,31 @@ public class AccessPointAdapter extends BaseAdapter {
         TextView levelItem = view.findViewById(R.id.level_in_detailed);
         levelItem.setText(context.getString(R.string.level_value, String.valueOf(itemData.level)));
 
+        ImageView levelImage = view.findViewById(R.id.levelImage_in_detailed);
+        Drawable picture;
+        if (itemData.level > -35) {
+            picture = context.getResources().getDrawable(context.getResources().getIdentifier("@drawable/ic_signal_wifi_4_bar", null, context.getPackageName()));
+            picture.setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_ATOP);
+            levelItem.setTextColor(Color.GREEN);
+        } else if (itemData.level > -50) {
+            picture = context.getResources().getDrawable(context.getResources().getIdentifier("@drawable/ic_signal_wifi_3_bar", null, context.getPackageName()));
+            picture.setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP);
+            levelItem.setTextColor(Color.YELLOW);
+        } else if (itemData.level > -75) {
+            picture = context.getResources().getDrawable(context.getResources().getIdentifier("@drawable/ic_signal_wifi_2_bar", null, context.getPackageName()));
+            picture.setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP);
+            levelItem.setTextColor(Color.YELLOW);
+        } else if (itemData.level > -90) {
+            picture = context.getResources().getDrawable(context.getResources().getIdentifier("@drawable/ic_signal_wifi_1_bar", null, context.getPackageName()));
+            picture.setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+            levelItem.setTextColor(Color.RED);
+        } else {
+            picture = context.getResources().getDrawable(context.getResources().getIdentifier("@drawable/ic_signal_wifi_0_bar", null, context.getPackageName()));
+            picture.setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+            levelItem.setTextColor(Color.RED);
+        }
+        levelImage.setImageDrawable(picture);
+
         TextView channelWidthItem = view.findViewById(R.id.width_in_detailed);
         channelWidthItem.setText(Utils.getChannelWidth(itemData) + " MHz");
 
@@ -100,10 +129,15 @@ public class AccessPointAdapter extends BaseAdapter {
             }
         } else if (fBand == Utils.FrequencyBand.FIVE_GHZ) {
             frequencyItemText = "5";
-            frequencyRangeItemText = " - ";
+            int frequency = 5000 + (Integer.parseInt(channel) * 5);
+            if (!channel.equals("1")) {
+                frequencyRangeItemText = (frequency - 20) + " - " + (frequency + 20);
+            } else {
+                frequencyRangeItemText = (frequency - 10) + " - " + (frequency + 10);
+            }
         } else if (fBand == Utils.FrequencyBand.SIX_GHZ) {
             frequencyItemText = "6";
-            frequencyRangeItemText = " - ";
+            frequencyRangeItemText = 5940 + " - " + 7100;
         }
         frequencyItem.setText(frequencyItemText + " GHz");
         frequencyRangeItem.setText(frequencyRangeItemText);
@@ -111,7 +145,7 @@ public class AccessPointAdapter extends BaseAdapter {
         TextView distanceItem = view.findViewById(R.id.distan_in_detailedce);
         double exp = (27.55 - (20 * Math.log10(Double.parseDouble(frequencyItemText))) + Math.abs(itemData.level)) / 20.0;
         double distanceM = Math.pow(10.0, exp);
-        distanceItem.setText("~" + String.valueOf(distanceM/1000).substring(0,5)+"m");
+        distanceItem.setText("~" + String.valueOf(distanceM / 1000).substring(0, 5) + "m");
 
         return view;
     }
